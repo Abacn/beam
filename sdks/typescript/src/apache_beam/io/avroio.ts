5e4ed7f9bb829a0df204212919dbafb3924e8b50
Yi Hu <yathu@google.com>
2022-09-08 11:53:56 -0400

    another change

--- sdks/typescript/src/apache_beam/io/avroio.ts
+++ sdks/typescript/src/apache_beam/io/avroio.ts
@@ -24,6 +24,7 @@ import { withCoderInternal } from "../transforms/internal";
 
 export function readFromAvro<T>(
   filePattern: string,
+  // TODO: Allow schema to be inferred.
   options: { schema: Schema }
 ): beam.AsyncPTransform<beam.Root, beam.PCollection<T>> {
   return schemaio<beam.Root, beam.PCollection<T>>(
